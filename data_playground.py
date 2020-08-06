5e689de2a8636d72437e681767588a48f422e99f
Michael Calabro <michael_calabro@college.harvard.edu>
2020-08-06 15:05:58 -0400

    more baby steps

--- data_playground.py
+++ data_playground.py
@@ -1,4 +1,11 @@
 import pandas as pd
+import numpy as np
+
+# Code below allows me to view entire table when I print it
+desired_width = 320
+pd.set_option('display.width', desired_width)
+np.set_printoptions(linewidth=desired_width)
+pd.set_option('display.max_columns', 30)
 
 # nba_data.csv downloaded from Kaggle
 nba = pd.read_csv("nba_data.csv", index_col=0)
