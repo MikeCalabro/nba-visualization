94d7c46dddefbddc467a5744a62b9f3ceaf73718
Michael Calabro <michael_calabro@college.harvard.edu>
2020-08-06 15:20:22 -0400

    baby steps

--- data_playground.py
+++ data_playground.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-# Code below allows me to view entire table when I print it
+# Code below allows me to view entire table when I print it (in PyCharm IDE)
 desired_width = 320
 pd.set_option('display.width', desired_width)
 np.set_printoptions(linewidth=desired_width)
@@ -10,4 +10,5 @@ pd.set_option('display.max_columns', 30)
 # nba_data.csv downloaded from Kaggle
 nba = pd.read_csv("nba_data.csv", index_col=0)
 
+# Shows the head and tail of the data in PyCharm
 print(nba)
