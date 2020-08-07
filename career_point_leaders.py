40edf6a41b8ca1432893efbbd86df628bce922fd
Michael Calabro <michael_calabro@college.harvard.edu>
2020-08-07 15:01:56 -0400

    rearranging (world is changing)

---
+++ career_point_leaders.py
@@ -0,0 +1,33 @@
+# Creates a bar graph of NBA career point leaders for players drafted after 1995
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+nba_clean = pd.read_csv("nba_clean.csv")
+
+print(nba_clean)
+
+# Creates a list of leading all time scorers whose careers started in `96 or later
+# Need to turn draft_year into an int first :p
+career_point_leaders = (
+      nba_clean
+      .loc[nba_clean.draft_year > 1995]
+      .loc[:, ['player_name', 'draft_year', 'total_points']]
+      .groupby(['player_name', 'draft_year'])
+      .sum()
+      .sort_values(by='total_points', ascending=False)
+      .iloc[0:15]
+      .rename(columns={'total_points': 'career_points'})
+      .reset_index()
+      )
+
+print(career_point_leaders)
+
+# Plots the career point leaders
+fig, ax = plt.subplots()
+ax.bar(x=career_point_leaders.player_name, height=career_point_leaders.career_points)
+ax.set_ylabel('Career Points')
+ax.set_title('Career Point Leaders For Players Drafted After 1995')
+# Code below tilts the x-axis names so they don't overlap
+fig.autofmt_xdate()
+plt.show()
--- data_playground.py
+++ data_playground.py
@@ -51,6 +51,8 @@ def draft_year_int(row):
 
 nba = nba.apply(draft_year_int, axis='columns')
 
+print(nba)
+
 # Makes a clean csv file to be used for visualizations
 nba.to_csv(path_or_buf="nba_clean.csv", index=False)
 
@@ -67,28 +69,3 @@ print(nba
       .apply(lambda group: group.loc[group.pts.idxmax()])
       .set_index('season')
       )
-
-# Creates a list of leading all time scorers whose careers started in `96 or later
-# Need to turn draft_year into an int first :p
-career_point_leaders = (
-      nba
-      .loc[nba.draft_year > 1995]
-      .loc[:, ['player_name', 'draft_year', 'total_points']]
-      .groupby(['player_name', 'draft_year'])
-      .sum()
-      .sort_values(by='total_points', ascending=False)
-      .iloc[0:15]
-      .rename(columns={'total_points': 'career_points'})
-      .reset_index()
-      )
-
-print(career_point_leaders)
-
-# Plots the career point leaders
-fig, ax = plt.subplots()
-ax.bar(x=career_point_leaders.player_name, height=career_point_leaders.career_points)
-ax.set_ylabel('Career Points')
-ax.set_title('Career Point Leaders For Players Drafted After 1995')
-# Code below tilts the x-axis names so they don't overlap
-fig.autofmt_xdate()
-plt.show()
