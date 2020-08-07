f637abaf9c5cf3eb1f44d1e64c3c5d1d9fe228f3
Michael Calabro <michael_calabro@college.harvard.edu>
2020-08-07 02:25:32 -0400

    mapping skillz

--- data_playground.py
+++ data_playground.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 
 # Code below allows me to view entire table when I print it (in PyCharm IDE)
 desired_width = 320
@@ -15,3 +16,14 @@ print(nba)
 
 # Select [all rows, [some columns]].arrange by points scored.print the top x columns
 print(nba.loc[:, ['player_name', 'season', 'pts']].sort_values(by='pts', ascending=False).iloc[0:30])
+
+
+# Function to fix the season column to be a single year int
+def fix_season(row):
+    row.season = int(row.season[0:4]) + 1
+    return row
+
+
+# Applies the function above to the nba dataframe
+nba = nba.apply(fix_season, axis='columns')
+
