31fbeb8707958ea6f6dfd564b5036a24f30676ce
Michael Calabro <michael_calabro@college.harvard.edu>
2020-08-08 17:19:20 -0400

    small progress

--- plot-scripts/league_points.py
+++ plot-scripts/league_points.py
@@ -1,9 +1,16 @@
-# Creates a line graph of total points scored in the NBA for each season since 1996/7
+# Creates a line graph of total points scored in the NBA for each team in each season since 1996/7
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
 nba_clean = pd.read_csv("~/PycharmProjects/pycharm-test/nba_clean.csv")
 
-print(nba_clean)
+league_points = (nba_clean
+                 .loc[:, ['team_abbreviation', 'season', 'total_points']]
+                 .groupby(['season', 'team_abbreviation'])
+                 .sum()
+                 .reset_index()
+                 )
+
+print(league_points)
 
