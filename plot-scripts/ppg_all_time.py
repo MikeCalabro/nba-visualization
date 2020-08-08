c3a5835bb18d141a5831dc301cf3cb215eaf0f8f
Michael Calabro <michael_calabro@college.harvard.edu>
2020-08-08 16:44:28 -0400

    new plot

--- plot-scripts/ppg_all_time.py
+++ plot-scripts/ppg_all_time.py
@@ -6,5 +6,27 @@ import matplotlib.pyplot as plt
 nba_clean = pd.read_csv("nba_clean.csv")
 
 
+# Easier to plot if the season is a string
+def string_season(row):
+    row.season = str(row.season)
+    return row
+
+
 # Select [all rows, [some columns]].sort by points_scored.print the top x (15) columns
-print(nba_clean.loc[:, ['player_name', 'season', 'pts']].sort_values(by='pts', ascending=False).iloc[0:15])
+ppg_all_time = (nba_clean
+                .loc[:, ['player_name', 'season', 'pts']]
+                .sort_values(by='pts', ascending=False)
+                .iloc[0:15]
+                .apply(string_season, axis='columns')
+                )
+
+print(ppg_all_time)
+
+fig, ax = plt.subplots()
+ax.bar(x=ppg_all_time.player_name + ' - ' + ppg_all_time.season,
+       height=ppg_all_time.pts,
+       color="C9")
+ax.set_ylabel("Points Per Game")
+ax.set_title("Highest NBA Season Points Per Game Since 1996/97")
+fig.autofmt_xdate()
+plt.show()
--- plot-scripts/ppg_by_season.py
+++ plot-scripts/ppg_by_season.py
@@ -5,11 +5,31 @@ import matplotlib.pyplot as plt
 
 nba_clean = pd.read_csv("nba_clean.csv")
 
+
+# Easier to plot if the season is a string
+def string_season(row):
+    row.season = str(row.season)
+    return row
+
+
 # Selects the name, pts per game, and assists per game, of the highest scoring player (per game) from each season
 # I don't know if this is the best way to organize the command, but it was getting long
-print(nba_clean
+ppg_by_season = (nba_clean
       .loc[:, ['player_name', 'season', 'pts', 'ast']]
       .groupby(['season'])
       .apply(lambda group: group.loc[group.pts.idxmax()])
-      .set_index('season')
+      .loc[:, ['player_name', 'pts', 'ast']]
+      .reset_index()
+      .apply(string_season, axis='columns')
       )
+
+print(ppg_by_season)
+
+fig, ax = plt.subplots()
+ax.bar(x=ppg_by_season.player_name + ' - ' + ppg_by_season.season,
+       height=ppg_by_season.pts,
+       color="C3")
+ax.set_ylabel("Points Per Game")
+ax.set_title("Highest NBA Points Per Game In Each Season Since 1996/97")
+fig.autofmt_xdate()
+plt.show()
