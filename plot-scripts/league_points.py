9d0d0391bac16d864565b2a560413c403b6acd54
Michael Calabro <michael_calabro@college.harvard.edu>
2020-08-10 11:15:25 -0400

    learning seaborn

--- plot-scripts/league_points.py
+++ plot-scripts/league_points.py
@@ -1,7 +1,7 @@
 # Creates a line graph of total points scored in the NBA for each team in each season since 1996/7
-
 import pandas as pd
-import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 nba_clean = pd.read_csv("~/PycharmProjects/pycharm-test/nba_clean.csv")
 
@@ -14,3 +14,14 @@ league_points = (nba_clean
 
 print(league_points)
 
+# Creates a list of every nba team that I can loop through
+nba_teams = league_points['team_abbreviation'].unique()
+
+# Honestly does not create a very interesting plot at all
+# However, I got a handle for using seaborn and looping through a list to make multiple lines
+for team in nba_teams[0:6]:
+    team_points = league_points.total_points[league_points.team_abbreviation == team]
+    sns.lineplot(league_points.season, team_points, label=team)
+plt.xlabel('Season')
+plt.ylabel('Team Points Scored')
+plt.show()
